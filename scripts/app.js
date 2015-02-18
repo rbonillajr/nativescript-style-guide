8457dd48a22722291bbdb13258525c0c5d2f26d2
TJ VanToll <tj.vantoll@gmail.com>
2015-02-18 17:00:42 -0500

    More pedantic cleanup

--- scripts/app.js
+++ scripts/app.js
@@ -1,59 +1,29 @@
-//setup splitter
-var splitter = $('#splitter').kendoSplitter({
-	panes: [
-		{ collapsible: true, size: "25%" },
-		{ collapsible: false}
-	]
-}).data('kendoSplitter');
+(function() {
+	"use strict";
 
-//create TOC
-jQuery('.toc').each(function(index){
+	var splitter = $( "#splitter" ).kendoSplitter({
+		panes: [
+			{ collapsible: true, size: "25%" },
+			{ collapsible: false }
+		]
+	}).data( "kendoSplitter" );
 
-	jQuery('h2','#toc'+(index+1)).each(function(i){
-		//$(this).attr('id',$(this).text().replace(/ /g,'-'));
-		$(this)
-			.nextUntil('h3')
-			.andSelf()
-			.wrapAll('<div class="sec" id="'+$(this).text().replace(/ /g,'-')+'"></div>');
-		$('#toc-padding')
-			.append('<h5><a href="#'+($(this).text().replace(/[ .]/g,'-'))+'">'+$(this).html()+'</a></h5>');
+	var toc = $( "#toc-padding" );
+	$( "div[id*=chapter]" ).each(function( chapterNumber ) {
+		$( "h2", "#chapter" + chapterNumber ).each(function( i ) {
+			toc.append( "<h5><a href='#chapter" + chapterNumber + "'>" +
+				$( this ).html() + "</a></h5>" );
+		});
+
+		$( "h3", "#chapter" + chapterNumber ).each(function( i ) {
+			var hash = chapterNumber + "." + ( i + 1 ) + "";
+			toc.append( "<h6><a href='#chapter" + chapterNumber + "." + ( i + 1 ) + "'>" +
+				$( this ).html() + "</a></h6>" );
+		});
 	});
 
-	jQuery('h3','#toc'+(index+1)).each(function(i){
-		//$(this).attr('id',$(this).text().replace(/ /g,'-'));
-		$(this)
-			.nextUntil('h3')
-			.andSelf()
-			.wrapAll('<div class="sec" id="'+$(this).text().replace(/ /g,'-')+'"></div>');
-		$('#toc-padding')
-			.append('<h6><a href="#'+($(this).text().replace(/[ .]/g,'-'))+'">'+$(this).html()+'</a></h6>');
+	$( "#toggle-toc" ).on( "click",function() {
+		splitter.toggle( ".k-pane:first" );
+		return false;
 	});
-	
-});
-
-jQuery('.chapter').each(function(index){
-	jQuery('h2','#chapter'+(index+1)).each(function(i){
-		var foo = index+1+'';
-		//$(this).attr('id',((index+1)+($(this).text().replace(foo,'').replace(/ /g,'-'))));
-		$(this)
-			.wrap('<div class="sec" id="'+((index+1)+($(this).text().replace(foo,'').replace(/ /g,'-')))+'"></div>');
-		$('#toc-padding')
-			.append('<h5><a href="#'+(index+1)+($(this).text().replace(foo,'').replace(/ /g,'-'))+'">'+$(this).html()+'</a></h5>');
-	});
-
-	jQuery('h3','#chapter'+(index+1)).each(function(i){
-		var foo = (index+1)+'.'+(i+1)+'';
-		//$(this).attr('id',(index+1)+'.'+(i+1)+($(this).text().replace(foo,'').replace(/ /g,'-')));
-		$(this)
-			.nextUntil('h3')
-			.andSelf()
-			.wrapAll('<div class="sec" id="'+(index+1)+'.'+(i+1)+($(this).text().replace(foo,'').replace(/ /g,'-'))+'"></div>');
-		$('#toc-padding').append('<h6><a href="#'+(index+1)+'.'+(i+1)+($(this).text().replace(foo,'').replace(/ /g,'-'))+'">'+$(this).html()+'</a></h6>');
-	});  
-});
-
-//toggle
-$( '#toggle-toc' ).on('click',function() {
-	splitter.toggle('.k-pane:first');
-	return false;
-});
+}());
