7692b179cc3e510acfad2f90f3cc4730fcb493ee
TJ VanToll <tj.vantoll@gmail.com>
2015-02-19 23:04:04 -0500

    Build the scripts too

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,22 @@
-var gulp = require( "gulp" );
+var gulp = require( "gulp" ),
+	concat = require( "gulp-concat" ),
 	sass = require( "gulp-sass" ),
 	watch = require( "gulp-watch" );
 
 gulp.task( "default", function() {
 	gulp.watch( "./scss/*.scss", [ "sass" ] );
+	gulp.watch( "./scripts/*.js", [ "scripts" ] );
 });
 
 gulp.task( "sass", function () {
 	gulp.src( "./scss/*.scss" )
 		.pipe( sass() )
 		.pipe( gulp.dest( "./css" ) );
+});
+
+gulp.task( "scripts", function() {
+	gulp.src([ "bower_components/jquery/dist/jquery.min.js", 
+		"scripts/prism.js", "scripts/app.js" ])
+		.pipe( concat( "built.js" ) )
+		.pipe( gulp.dest( "./scripts/" ) );
 });
